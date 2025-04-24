952e92b79b0dea18b96e59ced098f9a5888c719d
Manish.U <manish6singh2020@gmail.com>
2025-04-24 22:20:23 +0530

    data preprocessing and cleaning task1

---
+++ dayone.py
@@ -0,0 +1,13 @@
+#Data preprocessing and cleaning
+#step 1 import necessary libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+#step 2 Read dataset
+df=pd.read_csv("Titanic-Dataset.csv")
+df.head()
+
+
+
+
